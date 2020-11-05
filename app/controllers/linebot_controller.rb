bca59c2c4a84b83182bc30837c7c9ae00c05ff54
Mami <nozaru.yeah@gmail.com>
2020-11-05 19:50:32 +0900

    first commit

---
+++ app/controllers/linebot_controller.rb
@@ -0,0 +1,51 @@
+class LinebotController < ApplicationController
+    require 'line/bot'  # gem 'line-bot-api'
+  
+    # callbackアクションのCSRFトークン認証を無効
+    protect_from_forgery :except => [:callback]
+  
+    def client
+      @client ||= Line::Bot::Client.new { |config|
+        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      }
+    end
+  
+    def callback
+      body = request.body.read
+  
+      signature = request.env['HTTP_X_LINE_SIGNATURE']
+      unless client.validate_signature(body, signature)
+        head :bad_request
+      end
+  
+      events = client.parse_events_from(body)
+  
+      events.each { |event|
+  
+        # event.message['text']でLINEで送られてきた文書を取得
+        if event.message['text'].include?("好き")
+          response = "へーい"
+        elsif event.message["text"].include?("行ってきます")
+          response = "はーい"
+        else
+          response = "test"
+        end
+        #if文でresponseに送るメッセージを格納
+  
+        case event
+        when Line::Bot::Event::Message
+          case event.type
+          when Line::Bot::Event::MessageType::Text
+            message = {
+              type: 'text',
+              text: response
+            }
+            client.reply_message(event['replyToken'], message)
+          end
+        end
+      }
+  
+      head :ok
+    end
+  end
